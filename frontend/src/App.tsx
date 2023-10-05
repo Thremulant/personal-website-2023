330e8cd8a02a7939f7258c191edd5ae78ebcf018
thremulant <hi@danielpescador.com>
2023-10-04 23:19:24 -0500

    Initial commit for the frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="uk-inline">
+        <button className="uk-button uk-button-default" type="button">Hover</button>
+        <div data-uk-dropdown>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+      </div>
+    </>
+  )
+}
+
+export default App
