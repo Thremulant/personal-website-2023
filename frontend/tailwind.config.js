95f4b4c7a007e7012868fcaa6ebaf82e447fe54e
thremulant <hi@danielpescador.com>
2023-10-05 23:09:49 -0500

    configured Tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors';
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      main: colors.rose[600],
+      white: colors.stone[50],
+      black: colors.stone[950],
+    },
+    extend: {
+      fontFamily: {
+        'neue': ['"Neue Montreal"', 'Montserrat', 'sans-serif'],
+        'work': ['"Work Sans"', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
